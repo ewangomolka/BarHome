3dc2f6efba49bfc788209ad6910ebaf6d6e06879
ewangomolka <ewangomolka@gmail.com>
2023-07-19 16:18:54 +0100

    styling cards

--- BarHome Front/bar_home/src/components/CocktailDetail.js
+++ BarHome Front/bar_home/src/components/CocktailDetail.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './CocktailDetail.css';
 
 const CocktailDetail = ({cocktail}) => {
 
+    const [showIngredients, setShowIngredients] = useState(true)
+
     const cocktailIngredients = cocktail.ingredients.map((ingredient, index) => {
-        return <li key={index}>{ingredient.name} {ingredient.quantity}</li>
+        return <li key={index}><p>{ingredient.name}:</p><p>{ingredient.quantity}</p></li>
     })
 
+    const showCocktailIngredients = () => {
+        setShowIngredients(!showIngredients)
+    }
+
+    const handleIngredients = () => {
+        showCocktailIngredients()
+    }
+
     return ( 
-        <div>
-        <img src={cocktail.image}/>
-        <p>{cocktail.name}</p>
-        <ul>{cocktailIngredients}</ul>
-        <p>{cocktail.method}</p>
+        <div className='page-container'>
+            <div className='cocktail-container'>
+                <div>
+                    <p>{cocktail.name}</p>
+                    <p>{cocktail.type}</p>
+                    <img className='image' src={cocktail.image}/>
+                </div>
+                <div className='buttons'>
+                    {showIngredients ? <button onClick={handleIngredients}>Method</button> : 
+                    <button onClick={handleIngredients}>Ingredients</button>}
+                </div>
+                <div className='card' id='card'>
+                    { showIngredients ? <div className='cocktail-ingredients'>
+                        <ul>{cocktailIngredients}</ul>
+                    </div> : <div className='cocktail-method'>
+                        <p>{cocktail.method}</p>
+                    </div>}
+                </div>
+            </div>
         </div>
      );
 }
